41f5ebf526a924d295e5ce54d93a9b74d7591c98
Merna Saad <69446863+Merna257@users.noreply.github.com>
2024-12-05 15:09:52 -0800

    Add files via upload

---
+++ public/javascripts/clickActions.js
@@ -0,0 +1,23 @@
+function addProduct(){
+    window.location.href = '/products/add';
+}
+
+function cancelAdd(){
+    window.location.href = '/products';
+}
+
+function cancelGoHome(){
+    window.location.href = '/store';
+}
+function cancelDelete(){
+    window.location.href = '/products';
+}
+
+// Handle bar
+const burger = document.getElementById("navburger");
+const menu = document.getElementById("navbarBasicExample");
+const toggleBurger = () => {
+  burger.classList.toggle("is-active");
+  menu.classList.toggle("is-active");
+};
+burger.addEventListener("click", toggleBurger, false);
