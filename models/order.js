aa43347e9d5f6a41416960965c8df081b000b5b7
Merna Saad <69446863+Merna257@users.noreply.github.com>
2024-12-05 15:08:59 -0800

    Add files via upload

---
+++ models/order.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const orderSchema = mongoose.Schema(
+  {
+    sku: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    purchaseDate: {
+      type: Date,
+      required: true,
+    },
+    quantityOrdered: {
+      type: Number,
+      required: true,
+    },
+    invoiceTotal: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    collection: "orders_rochon",
+  }
+);
+const orderModel = mongoose.model("OrderModel", orderSchema);
+module.exports = orderModel;
---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users_rochon",
+  }
+);
+const userModel = mongoose.model("UserModel", userSchema);
+module.exports = userModel;
